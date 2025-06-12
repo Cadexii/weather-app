7694963d19ba77b57cede192a2f20f88df24670d
Cadexii <cadexiipl@gmail.com>
2025-06-12 12:04:24 +0200

    create root page hub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+import { useAuth } from "@/app/components/Contexts/AuthProvider";
+import { useRouter } from "next/navigation";
+import { Icon } from "@iconify/react";
+
 export default function Home() {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/dashboard");
+    } else {
+      router.push("/login");
+    }
+  }, [user, router]);
+
   return (
-    <div>
-      <h2>Root Page</h2>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "90vh",
+      }}
+    >
+      <Icon icon="svg-spinners:270-ring-with-bg" color="#888" width={50} />
     </div>
   );
 }
