8629929cfaf23dfe92fa698a8bd007bcde9eb7e7
Cadexii <cadexiipl@gmail.com>
2025-06-12 11:04:01 +0200

    update auth context

--- src/app/components/Contexts/AuthProvider.tsx
+++ src/app/components/Contexts/AuthProvider.tsx
@@ -1,15 +1,27 @@
 "use client";
 
 import { createContext, useContext, useEffect, useState } from "react";
-import { onAuthStateChanged, User } from "firebase/auth";
+import {
+  onAuthStateChanged,
+  User,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../../utils/firebaseConfig";
 
 type Props = {
   user: User | null;
   loading: boolean;
+  signIn: (email: string, password: string) => Promise<void>;
+  logOut: () => Promise<void>;
 };
 
-const AuthContext = createContext<Props>({ user: null, loading: true });
+const AuthContext = createContext<Props>({
+  user: null,
+  loading: true,
+  signIn: async () => {},
+  logOut: async () => {},
+});
 
 export const useAuth = () => useContext(AuthContext);
 
@@ -25,8 +37,16 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     return unsubscribe;
   }, []);
 
+  const signIn = async (email: string, password: string) => {
+    await signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = async () => {
+    await signOut(auth);
+  };
+
   return (
-    <AuthContext.Provider value={{ user, loading }}>
+    <AuthContext.Provider value={{ user, loading, signIn, logOut }}>
       {children}
     </AuthContext.Provider>
   );
