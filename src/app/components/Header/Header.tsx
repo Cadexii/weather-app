f1503b2f0c3d9e0da0baa587d8d41707af4c98f4
Cadexii <cadexiipl@gmail.com>
2025-06-12 12:26:39 +0200

    remove header items if user is not logged in

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -12,7 +12,7 @@ const navigationItems = [
 
 const Header = () => {
   const pathname = usePathname();
-  const { logOut } = useAuth();
+  const { user, loading, logOut } = useAuth();
 
   const getLinkClass = (path: string) => {
     return `${styles.item} ${pathname === path && styles.isActive}`;
@@ -21,17 +21,23 @@ const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.itemsContainer}>
-        {navigationItems.map((item) => (
-          <Link key={item.name} href={item.href}>
-            <p className={getLinkClass(item.href)}>{item.name}</p>
-          </Link>
-        ))}
+        {user &&
+          !loading &&
+          navigationItems.map((item) => (
+            <Link key={item.name} href={item.href}>
+              <p className={getLinkClass(item.href)}>{item.name}</p>
+            </Link>
+          ))}
       </div>
-      <div className={styles.itemsContainer}>
-        <p className={styles.item} onClick={logOut}>
-          Log out
-        </p>
-      </div>
+      {user && !loading ? (
+        <div className={styles.itemsContainer}>
+          <p className={styles.item} onClick={logOut}>
+            Log out
+          </p>
+        </div>
+      ) : (
+        <p className={styles.titleItem}>Weather App</p>
+      )}
     </div>
   );
 };
