69f4404f3b16752729939ec008cb94a8de92da5c
Cadexii <cadexiipl@gmail.com>
2025-06-12 10:08:55 +0200

    create auth context

---
+++ src/app/components/Contexts/AuthProvider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../../utils/firebaseConfig";
+
+type Props = {
+  user: User | null;
+  loading: boolean;
+};
+
+const AuthContext = createContext<Props>({ user: null, loading: true });
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
